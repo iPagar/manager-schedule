be9063205fbd2b54f5d30914a38fec7276b8d5f9
BuildTools <ipagar@icloud.com>
2022-02-09 22:56:08 +0300

    add parser feature

--- services/parser.js
+++ services/parser.js
@@ -1,83 +1,76 @@
 const fs = require("fs");
-const parser = require("rector-schedule-parser");
+const { parser } = require("rector-schedule-parser");
 const db = require("./mongoDriver");
 
 function parseFile(path) {
-	return parser(path).then((lessons) => {
-		return lessons
-			.map((lesson) => {
-				const {
-					stgroup,
-					subject,
-					audience,
-					group,
-					teacher,
-					type,
-					start_time,
-				} = lesson;
+  return parser(path).then((lessons) => {
+    return lessons
+      .map((lesson) => {
+        const { stgroup, subject, audience, group, teacher, type, start_time } =
+          lesson;
 
-				const oneDay = lesson.dates.map((date) => {
-					//год, месяц(0 - 11), день, часы, минуты
-					const newDate = new Date(
-						new Date(Date.now()).getFullYear(),
-						date.match(/\d{2}$/) - 1,
-						date.match(/^\d{2}/),
-						start_time.match(/^\d{1,2}/)[0],
-						start_time.match(/\d{1,2}$/)[0]
-					);
+        const oneDay = lesson.dates.map((date) => {
+          //год, месяц(0 - 11), день, часы, минуты
+          const newDate = new Date(
+            new Date(Date.now()).getFullYear(),
+            date.match(/\d{2}$/) - 1,
+            date.match(/^\d{2}/),
+            start_time.match(/^\d{1,2}/)[0],
+            start_time.match(/\d{1,2}$/)[0]
+          );
 
-					return {
-						stgroup,
-						subject,
-						audience,
-						group,
-						teacher,
-						type,
-						start_date: newDate,
-						end_date: newDate,
-						repeat: "once",
-					};
-				});
+          return {
+            stgroup,
+            subject,
+            audience,
+            group,
+            teacher,
+            type,
+            start_date: newDate,
+            end_date: newDate,
+            repeat: "once",
+          };
+        });
 
-				const repeatDay = lesson.periods.map((period) => {
-					const { start_date, end_date, repeat } = period;
+        const repeatDay = lesson.periods.map((period) => {
+          const { start_date, end_date, repeat } = period;
 
-					//год, месяц(0 - 11), день, часы, минуты
-					const startDate = new Date(
-						new Date(Date.now()).getFullYear(),
-						start_date.match(/\d{2}$/) - 1,
-						start_date.match(/^\d{2}/)[0],
-						start_time.match(/^\d{1,2}/)[0],
-						start_time.match(/\d{1,2}$/)[0]
-					);
+          //год, месяц(0 - 11), день, часы, минуты
+          const startDate = new Date(
+            new Date(Date.now()).getFullYear(),
+            start_date.match(/\d{2}$/) - 1,
+            start_date.match(/^\d{2}/)[0],
+            start_time.match(/^\d{1,2}/)[0],
+            start_time.match(/\d{1,2}$/)[0]
+          );
 
-					const endDate = new Date(
-						new Date(Date.now()).getFullYear(),
-						end_date.match(/\d{2}$/) - 1,
-						end_date.match(/^\d{2}/)[0],
-						start_time.match(/^\d{1,2}/)[0],
-						start_time.match(/\d{1,2}$/)[0]
-					);
+          const endDate = new Date(
+            new Date(Date.now()).getFullYear(),
+            end_date.match(/\d{2}$/) - 1,
+            end_date.match(/^\d{2}/)[0],
+            start_time.match(/^\d{1,2}/)[0],
+            start_time.match(/\d{1,2}$/)[0]
+          );
 
-					// console.log(lesson, startDate.toString());
+          // console.log(lesson, startDate.toString());
 
-					return {
-						stgroup,
-						subject,
-						audience,
-						group,
-						teacher,
-						type,
-						start_date: startDate,
-						end_date: endDate,
-						repeat,
-					};
-				});
+          return {
+            stgroup,
+            subject,
+            audience,
+            group,
+            teacher,
+            type,
+            start_date: startDate,
+            end_date: endDate,
+            repeat,
+          };
+        });
 
-				return [...oneDay, ...repeatDay];
-			})
-			.flat();
-	});
+        return [...oneDay, ...repeatDay];
+      })
+      .flat();
+  });
 }
 
 // parseFile("./schedules/МДС-20-01.pdf").then((r) => {
@@ -85,47 +78,47 @@ function parseFile(path) {
 // });
 
 function fileToBuffer(path) {
-	return new Promise((resolve, reject) => {
-		// const stream = fs.createReadStream(path);
-		// stream.setEncoding("binary");
+  return new Promise((resolve, reject) => {
+    // const stream = fs.createReadStream(path);
+    // stream.setEncoding("binary");
 
-		// let data = "";
+    // let data = "";
 
-		// stream.on("readable", function() {
-		// 	let chunk = stream.read();
+    // stream.on("readable", function() {
+    // 	let chunk = stream.read();
 
-		// 	if (chunk !== null) data += chunk;
-		// });
+    // 	if (chunk !== null) data += chunk;
+    // });
 
-		// stream.on("end", function() {
-		// 	resolve(data);
-		// });
+    // stream.on("end", function() {
+    // 	resolve(data);
+    // });
 
-		// stream.on("error", function(err) {
-		// 	if (err.code == "ENOENT") {
-		// 		reject("Файл не найден");
-		// 	} else {
-		// 		reject(err);
-		// 	}
-		// });
+    // stream.on("error", function(err) {
+    // 	if (err.code == "ENOENT") {
+    // 		reject("Файл не найден");
+    // 	} else {
+    // 		reject(err);
+    // 	}
+    // });
 
-		fs.readFile(path, function(err, data) {
-			if (err) throw err;
+    fs.readFile(path, function (err, data) {
+      if (err) throw err;
 
-			// console.log(data.toString("base64"));
-			resolve(data.toString("base64"));
-		});
-	});
+      // console.log(data.toString("base64"));
+      resolve(data.toString("base64"));
+    });
+  });
 }
 
 function getFile(path) {
-	const filename = path.match(/(?<name>[А-яа-я\d-() ]*)\.pdf/).groups.name;
+  const filename = path.match(/(?<name>[А-яа-я\d-() ]*)\.pdf/).groups.name;
 
-	return fileToBuffer(path).then((file) => {
-		return parseFile(path).then((parsed) => {
-			return { filename, file, parsed };
-		});
-	});
+  return fileToBuffer(path).then((file) => {
+    return parseFile(path).then((parsed) => {
+      return { filename, file, parsed };
+    });
+  });
 }
 
 module.exports = getFile;
