ef5f3ea7e02454142fed9d64a02b4e9b013d9a80
iPagar <p.garaev@beetbarrel.ru>
2023-03-21 01:10:01 +0400

    update

--- index.js
+++ index.js
@@ -3,55 +3,55 @@ const fs = require("fs");
 const moodle = require("./services/moodleFetcher");
 const parser = require("./services/parser");
 const {
-	insertLessons,
-	insertFiles,
-	getFile,
-	insertFavourite,
-	removeFavourite,
-	getFavourites,
-	getLessons,
-	getStgroup,
-	getGroups,
-	drop,
+  insertLessons,
+  insertFiles,
+  getFile,
+  insertFavourite,
+  removeFavourite,
+  getFavourites,
+  getLessons,
+  getStgroup,
+  getGroups,
+  drop,
 } = require("./services/mongoDriver");
 const rimraf = require("rimraf");
 
 const dir = `./schedules`;
 
 function update(courseExp, folderExp) {
-	return moodle(dir, courseExp, folderExp)
-		.then(async (files) => {
-			const promises = files.map((filename) =>
-				parser(`${dir}/${filename}`)
-			);
-			let lessons = [];
-			const pdfs = [];
+  return moodle(dir, courseExp, folderExp)
+    .then(async (files) => {
+      const promises = files.map((filename) => parser(`${dir}/${filename}`));
+      let lessons = [];
+      const pdfs = [];
 
-			await Promise.all(promises).then((schedules) =>
-				schedules.map((schedule) => {
-					const { filename, file, parsed } = schedule;
+      await Promise.all(promises).then((schedules) =>
+        schedules.map((schedule) => {
+          const { filename, file, parsed } = schedule;
 
-					lessons = lessons.concat(parsed);
-					pdfs.push({ name: filename, file });
-				})
-			);
+          lessons = lessons.concat(parsed);
+          pdfs.push({ name: filename, file });
+        })
+      );
 
-			return { pdfs, lessons };
-		})
-		.then(async (resp) => {
-			await insertFiles(resp.pdfs);
-			return insertLessons(resp.lessons);
-		})
-		.catch((e) => console.log(e));
+      return { pdfs, lessons };
+    })
+    .then(async (resp) => {
+      await insertFiles(resp.pdfs);
+      return insertLessons(resp.lessons);
+    })
+    .catch((e) => console.log(e));
 }
 
+update(new RegExp(/Бакалавриат/), new RegExp(/курс/), new RegExp(/426732/));
+
 module.exports = {
-	drop,
-	update,
-	getFile,
-	insertFavourite,
-	getFavourites,
-	getLessons,
-	getStgroup,
-	getGroups,
+  drop,
+  update,
+  getFile,
+  insertFavourite,
+  getFavourites,
+  getLessons,
+  getStgroup,
+  getGroups,
 };
--- services/mongoDriver.js
+++ services/mongoDriver.js
@@ -9,234 +9,218 @@ const dbName = "stankin";
 
 // Use connect method to connect to the server
 const mongo = new MongoClient.connect(url, {
-	useUnifiedTopology: true,
+  useUnifiedTopology: true,
 }).then((client) => {
-	const db = client.db(dbName);
+  const db = client.db(dbName);
 
-	return db;
+  return db;
 });
 
-const insertLessons = function(lessons) {
-	return mongo.then(async (client) => {
-		return client.collection("lessons").insertMany(lessons);
-	});
+const insertLessons = function (lessons) {
+  return mongo.then(async (client) => {
+    return client.collection("lessons").insertMany(lessons);
+  });
 };
 
-const drop = function() {
-	return mongo.then(async (client) => {
-		await client
-			.collection("lessons")
-			.drop()
-			.catch((err) => console.error(`Fatal error occurred: ${err}`));
+const drop = function () {
+  return mongo.then(async (client) => {
+    await client
+      .collection("lessons")
+      .drop()
+      .catch((err) => console.error(`Fatal error occurred: ${err}`));
 
-		await client
-			.collection("files")
-			.drop()
-			.catch((err) => console.error(`Fatal error occurred: ${err}`));
-	});
+    await client
+      .collection("files")
+      .drop()
+      .catch((err) => console.error(`Fatal error occurred: ${err}`));
+  });
 };
 
-const insertFiles = function(files) {
-	return mongo.then(async (client) => {
-		return client.collection("files").insertMany(files);
-	});
+const insertFiles = function (files) {
+  return mongo.then(async (client) => {
+    return client.collection("files").insertMany(files);
+  });
 };
 
-const getGroups = function(stgroup) {
-	return mongo.then(async (client) => {
-		return client
-			.collection("lessons")
-			.distinct("group", { stgroup })
-			.catch((err) => console.error(`Fatal error occurred: ${err}`));
-	});
+const getGroups = function (stgroup) {
+  return mongo.then(async (client) => {
+    return client
+      .collection("lessons")
+      .distinct("group", { stgroup })
+      .catch((err) => console.error(`Fatal error occurred: ${err}`));
+  });
 };
 
-const insertFavourite = function(id, stgroup, group) {
-	return mongo.then((client) => {
-		return client
-			.collection("favourites")
-			.updateOne({ id }, { $set: { stgroup, group } }, { upsert: true });
-	});
+const insertFavourite = function (id, stgroup, group) {
+  return mongo.then((client) => {
+    return client
+      .collection("favourites")
+      .updateOne({ id }, { $set: { stgroup, group } }, { upsert: true });
+  });
 };
 
-const getFavourites = function(id) {
-	return mongo.then((client) =>
-		client.collection("favourites").findOne({ id })
-	);
+const getFavourites = function (id) {
+  return mongo.then((client) =>
+    client.collection("favourites").findOne({ id })
+  );
 };
 
-const getFile = function(stgroup) {
-	return mongo.then((client) =>
-		client.collection("files").findOne({ name: stgroup })
-	);
+const getFile = function (stgroup) {
+  return mongo.then((client) =>
+    client.collection("files").findOne({ name: stgroup })
+  );
 };
 
-const getStgroup = function(stgroup = "") {
-	return mongo.then((client) =>
-		client
-			.collection("files")
-			.find(
-				{ name: { $regex: `${stgroup.trim()}`, $options: "i" } },
-				{ projection: { name: 1 } }
-			)
-			.toArray()
-	);
+const getStgroup = function (stgroup = "") {
+  return mongo.then((client) =>
+    client
+      .collection("files")
+      .find(
+        { name: { $regex: `${stgroup.trim()}`, $options: "i" } },
+        { projection: { name: 1 } }
+      )
+      .toArray()
+  );
 };
 
-const getLessons = async function(
-	stgroup,
-	group = "Без подгруппы",
-	today = new Date(
-		new Date(Date.now()).getFullYear(),
-		new Date(Date.now()).getMonth(),
-		new Date(Date.now()).getDate()
-	)
+const getLessons = async function (
+  stgroup,
+  group = "Без подгруппы",
+  today = new Date(
+    new Date(Date.now()).getFullYear(),
+    new Date(Date.now()).getMonth(),
+    new Date(Date.now()).getDate()
+  )
 ) {
-	const todayFullYear = today.getFullYear();
-	const todayMonth = today.getMonth() + 1;
-	const todayDate = today.getDate();
-	const todayDay = today.getDay() + 1;
+  const todayFullYear = today.getFullYear();
+  const todayMonth = today.getMonth() + 1;
+  const todayDate = today.getDate();
+  const todayDay = today.getDay() + 1;
 
-	const tomorrow = new Date(today);
-	tomorrow.setDate(today.getDate() + 1);
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
 
-	const oneDay = await mongo.then((client) =>
-		client
-			.collection("lessons")
-			.find({
-				$and: [
-					{ stgroup },
-					{ $or: [{ group }, { group: "Без подгруппы" }] },
-					{ repeat: "once" },
-					{ start_date: { $gte: today } },
-					{ start_date: { $lte: tomorrow } },
-				],
-			})
-			.toArray()
-	);
+  const oneDay = await mongo.then((client) =>
+    client
+      .collection("lessons")
+      .find({
+        $and: [
+          { stgroup },
+          { $or: [{ group }, { group: "Без подгруппы" }] },
+          { repeat: "once" },
+          { start_date: { $gte: today } },
+          { start_date: { $lte: tomorrow } },
+        ],
+      })
+      .toArray()
+  );
 
-	const everyWeek = await mongo.then((client) =>
-		client
-			.collection("lessons")
-			.aggregate([
-				{
-					$match: {
-						$and: [
-							{ stgroup },
-							{ $or: [{ group }, { group: "Без подгруппы" }] },
-							{ repeat: "к.н." },
-							{
-								$expr: {
-									$and: [
-										{ $gte: [tomorrow, "$start_date"] },
-										{ $lte: [today, "$end_date"] },
-										{
-											$eq: [
-												todayFullYear,
-												{ $year: "$start_date" },
-											],
-										},
-										{
-											$and: [
-												{
-													$gte: [
-														todayMonth,
-														{
-															$month:
-																"$start_date",
-														},
-													],
-												},
-												{
-													$lte: [
-														todayMonth,
-														{ $month: "$end_date" },
-													],
-												},
-											],
-										},
-										{
-											$eq: [
-												{ $dayOfWeek: "$start_date" },
-												todayDay,
-											],
-										},
-									],
-								},
-							},
-						],
-					},
-				},
-			])
-			.toArray()
-	);
+  const everyWeek = await mongo.then((client) =>
+    client
+      .collection("lessons")
+      .aggregate([
+        {
+          $match: {
+            $and: [
+              { stgroup },
+              { $or: [{ group }, { group: "Без подгруппы" }] },
+              { repeat: "к.н." },
+              {
+                $expr: {
+                  $and: [
+                    { $gte: [tomorrow, "$start_date"] },
+                    { $lte: [today, "$end_date"] },
+                    {
+                      $eq: [todayFullYear, { $year: "$start_date" }],
+                    },
+                    {
+                      $and: [
+                        {
+                          $gte: [
+                            todayMonth,
+                            {
+                              $month: "$start_date",
+                            },
+                          ],
+                        },
+                        {
+                          $lte: [todayMonth, { $month: "$end_date" }],
+                        },
+                      ],
+                    },
+                    {
+                      $eq: [{ $dayOfWeek: "$start_date" }, todayDay],
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      ])
+      .toArray()
+  );
 
-	// console.log(Math.abs((new Date("2020-10-23T15:00:00.000Z") - today)) % 1209600000)
-	// console.log((new Date("2020-10-01T09:20:00.000Z")).toString())
-	// console.log(Math.abs((new Date("2020-10-01T09:20:00.000Z") - today)) % 1209600000)
-	const overWeek = await mongo.then((client) =>
-		client
-			.collection("lessons")
-			.aggregate([
-				{
-					$match: {
-						$and: [
-							{ stgroup },
-							{ $or: [{ group }, { group: "Без подгруппы" }] },
-							{ repeat: "ч.н." },
-							{
-								$expr: {
-									$and: [
-										{ $gte: [tomorrow, "$start_date"] },
-										{ $lte: [today, "$end_date"] },
-										{
-											$lte: [
-												{
-													$mod: [
-														{
-															$abs: {
-																$subtract: [
-																	"$start_date",
-																	tomorrow,
-																],
-															},
-														},
-														1209600000,
-													],
-												},
-												86400000,
-											],
-										},
+  // console.log(Math.abs((new Date("2020-10-23T15:00:00.000Z") - today)) % 1209600000)
+  // console.log((new Date("2020-10-01T09:20:00.000Z")).toString())
+  // console.log(Math.abs((new Date("2020-10-01T09:20:00.000Z") - today)) % 1209600000)
+  const overWeek = await mongo.then((client) =>
+    client
+      .collection("lessons")
+      .aggregate([
+        {
+          $match: {
+            $and: [
+              { stgroup },
+              { $or: [{ group }, { group: "Без подгруппы" }] },
+              { repeat: "ч.н." },
+              {
+                $expr: {
+                  $and: [
+                    { $gte: [tomorrow, "$start_date"] },
+                    { $lte: [today, "$end_date"] },
+                    {
+                      $lte: [
+                        {
+                          $mod: [
+                            {
+                              $abs: {
+                                $subtract: ["$start_date", tomorrow],
+                              },
+                            },
+                            1209600000,
+                          ],
+                        },
+                        86400000,
+                      ],
+                    },
 
-										{
-											$eq: [
-												{ $dayOfWeek: "$start_date" },
-												todayDay,
-											],
-										},
-									],
-								},
-							},
-						],
-					},
-				},
-			])
-			.toArray()
-	);
+                    {
+                      $eq: [{ $dayOfWeek: "$start_date" }, todayDay],
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      ])
+      .toArray()
+  );
 
-	return new Promise((resolve) =>
-		resolve([...oneDay, ...everyWeek, ...overWeek])
-	);
+  return new Promise((resolve) =>
+    resolve([...oneDay, ...everyWeek, ...overWeek])
+  );
 };
 
 module.exports = {
-	drop,
-	insertLessons,
-	insertFiles,
-	getFile,
-	insertFavourite,
-	getFavourites,
-	getLessons,
-	getStgroup,
-	getGroups,
+  drop,
+  insertLessons,
+  insertFiles,
+  getFile,
+  insertFavourite,
+  getFavourites,
+  getLessons,
+  getStgroup,
+  getGroups,
 };
